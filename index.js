6e46a9a48ba98902425bd2c4eca50fe2ac7f5130
goguntimehin <47008777+goguntimehin@users.noreply.github.com>
2020-08-21 03:22:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 
-var numberButton = document.querySelectorAll(".click-button").length;
 
-for (var i=0; i<numberButton; i++){
     document.querySelector(".click-button").addEventListener("click", function () {
         var audio = new Audio("sound\\King_Sunny_Ade_-_King_Sunny_Ade_His_African_Beats_-_Baba_Orun_A_Mbe_O_Latest_Yoruba_JuJu_Song_2020_.mp3");
         audio.play();
     });
 
-}
+
 
 
 
