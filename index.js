7d690be093629b1f0e895576aaf2d5e7254340c7
goguntimehin <47008777+goguntimehin@users.noreply.github.com>
2020-08-21 14:15:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+    var numberOfButton = document.querySelectorAll(".click-button").length;
+    for (var i = 0; i < numberOfButton; i++) {
+        document.querySelectorAll(".click-button")[i].addEventListener("click", function () {
+
+            var myButtonInnerHTML = this.innerHTML;
+
+            switch (myButtonInnerHTML) {
+                case "Click Dad's Song":
+                    var king = new Audio("sound\\King_Sunny_Ade_-_King_Sunny_Ade_His_African_Beats_-_Baba_Orun_A_Mbe_O_Latest_Yoruba_JuJu_Song_2020_.mp3");
+                    king.play();
+                    break;
 
 
-    document.querySelector(".click-button").addEventListener("click", function () {
-        var audio = new Audio("sound\\King_Sunny_Ade_-_King_Sunny_Ade_His_African_Beats_-_Baba_Orun_A_Mbe_O_Latest_Yoruba_JuJu_Song_2020_.mp3");
-        audio.play();
-    });
+                case "Click Dad Song":
+                    var audio = new Audio("sound\\Tope_Alabi_-_Tope_Alabi_-_Olorun_Gbangba_Angel_Mi_Latest_Yoruba_Gospel_Song_2020_.mp3");
+                    audio.play();
+                    break;
+
+                default:
+
+
+            }
+
+
+
+        });
+    }
 
 
 
